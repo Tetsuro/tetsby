6ac1f1a7f81cf0228d0dc68c69fe4f2c49080171
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2019-05-11 17:28:21 -0400

    Adds gatsby-source-wordpress, linting rules, and dotenv for environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,51 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: process.env.GATSBY_WP_BASE_URL,
+        protocol: process.env.GATSBY_WP_PROTOCOL,
+        hostingWPCOM: false,
+        useACF: false,
+        acfOptionPageIds: [],
+        auth: {
+          htaccess_user: "your-htaccess-username",
+          htaccess_pass: "your-htaccess-password",
+          htaccess_sendImmediately: false,
+          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
+          wpcom_app_clientId: "54793",
+          wpcom_user: "gatsbyjswpexample@gmail.com",
+          wpcom_pass: process.env.WORDPRESS_PASSWORD,
+          jwt_user: process.env.JWT_USER,
+          jwt_pass: process.env.JWT_PASSWORD,
+          jwt_base_path: "/jwt-auth/v1/token", // Default - can skip if you are using https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/
+        },
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://source-url.com",
+          replacementUrl: "https://replacement-url.com",
+        },
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+        ],
+        excludedRoutes: ["**/posts/1456"],
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
+  ],
 }
