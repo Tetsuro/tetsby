2cef019a777750ee3d0711474ffb1ddbdf510b88
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2019-05-18 20:14:38 -0400

    Finish styling

---
+++ src/components/MainMenu/MainMenu.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link, StaticQuery, graphql } from 'gatsby';
+
+import styles from './MainMenu.module.scss';
+
+class MainMenu extends Component {
+  render() {
+    const { links } = this.props;
+
+    const linksMarkup = links.map(link => (
+      <Link
+        to={link.url}
+        key={link.wordpress_id}
+        className={styles.MainMenuLink}
+      >
+        {link.title}
+      </Link>
+    ));
+
+    return <nav className={styles.MainMenu}>{linksMarkup}</nav>;
+  }
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      {
+        wordpressWpApiMenusMenusItems {
+          items {
+            title
+            url
+            wordpress_id
+          }
+        }
+      }
+    `}
+    render={data => (
+      <MainMenu links={data.wordpressWpApiMenusMenusItems.items} />
+    )}
+  />
+);
