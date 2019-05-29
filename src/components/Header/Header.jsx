4da4db1e117ec3d6c7cc04c886011721535aa909
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2019-05-28 20:27:47 -0400

    Convert Header into a function component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,24 +7,19 @@ import styles from './Header.module.scss';
 import logo from '../../images/tetchi-burger.gif';
 import darkLogo from '../../images/tetchi-burger-dark.gif';
 
-class Header extends Component {
-  render() {
-    const { title, description, displayMode } = this.props;
-    const logoSrc = displayMode === 'light' ? logo : darkLogo;
+export default function Header({ title, description, displayMode }) {
+  const logoSrc = displayMode === 'light' ? logo : darkLogo;
 
-    return (
-      <header className={styles.Header}>
-        <Link className={styles.HeaderHomeLink} to="/">
-          <h1 className={styles.Heading}>{title}</h1>
-          <img src={logoSrc} className={styles.Logo} />
-        </Link>
-        <div className={styles.HeaderNav}>
-          <MainMenu />
-          <h4 className={styles.HeaderDescription}>{description}</h4>
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className={styles.Header}>
+      <Link className={styles.HeaderHomeLink} to="/">
+        <h1 className={styles.Heading}>{title}</h1>
+        <img src={logoSrc} className={styles.Logo} />
+      </Link>
+      <div className={styles.HeaderNav}>
+        <MainMenu />
+        <h4 className={styles.HeaderDescription}>{description}</h4>
+      </div>
+    </header>
+  );
 }
-
-export default Header;
