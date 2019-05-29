f7a7e0463018c563ec7ac8faeae265738dbb682e
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2019-05-28 20:32:11 -0400

    Convert MainMenu into a function component

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
-import { Link, StaticQuery, graphql } from 'gatsby';
+import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 import styles from './MainMenu.module.scss';
 
-class MainMenu extends Component {
-  render() {
-    const { links } = this.props;
-
-    const linksMarkup = links.map((link, index) => (
-      <Link
-        to={index === 0 ? '/' : `${link.url}/`}
-        key={link.wordpress_id}
-        className={styles.MainMenuLink}
-        activeClassName={styles.MainMenuLinkIsActive}
-        // partiallyActive={index === 0}
-      >
-        {link.title}
-      </Link>
-    ));
-
-    return <nav className={styles.MainMenu}>{linksMarkup}</nav>;
-  }
-}
-
-export default () => (
-  <StaticQuery
-    query={graphql`
-      {
+export default function MainMenu() {
+  const {
+    wordpressWpApiMenusMenusItems: { items: links },
+  } = useStaticQuery(
+    graphql`
+      query {
         wordpressWpApiMenusMenusItems {
           items {
             title
@@ -35,9 +17,22 @@ export default () => (
           }
         }
       }
-    `}
-    render={data => (
-      <MainMenu links={data.wordpressWpApiMenusMenusItems.items} />
-    )}
-  />
-);
+    `
+  );
+
+  console.log(links);
+
+  const linksMarkup = links.map((link, index) => (
+    <Link
+      to={index === 0 ? '/' : `${link.url}/`}
+      key={link.wordpress_id}
+      className={styles.MainMenuLink}
+      activeClassName={styles.MainMenuLinkIsActive}
+      // partiallyActive={index === 0}
+    >
+      {link.title}
+    </Link>
+  ));
+
+  return <nav className={styles.MainMenu}>{linksMarkup}</nav>;
+}
