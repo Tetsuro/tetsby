aea38d8b59756cd6454eb16e25feda58ce100a52
Tetsuro Takara <tetchi@Tetsuros-Mac-mini.local>
2019-12-11 21:50:35 -0500

    WIP: Add tests

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import Header from '../header';
-describe('Header', () => {
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(<Header siteTitle="Default Starter" />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+import { render } from '@testing-library/react';
+
+import Header from './Header';
+// You have to write data-testid
+const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>;
+
+test('Displays the correct title', () => {
+  const { getByTestId } = render(<Title />);
+  // Assertion
+  expect(getByTestId('hero-title')).toHaveTextContent('Gatsby is awesome!');
+  // --> Test will pass
 });
