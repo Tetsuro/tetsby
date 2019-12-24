88d6e328e4aab8ae614d4367fbbc6bb50fddc17c
Tetsuro <tetsuro.takara@gmail.com>
2019-12-24 16:57:49 -0500

    Add a dark mode for code blocks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 require('./src/stylesheets/wp-modifiers.scss');
 require('./src/vendors/prism/prism.css');
+require('./src/vendors/prism/prism-dark.css');
 require('./src/vendors/prism/prism');
 
 exports.onRouteUpdate = () => {
   Prism.highlightAll();
-}
+};
