270d5a2eacb085155c5ace8bd9d2321c311da415
Tetsuro <tetsuro.takara@gmail.com>
2020-02-25 12:08:12 -0500

    Add test script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 let activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
 
-console.log('ENV', activeEnv);
-
 require('dotenv').config({
   path: `.env.${activeEnv}`,
 });
